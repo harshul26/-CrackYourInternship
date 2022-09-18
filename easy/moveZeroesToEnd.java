f96e479835ff492b66487aea5bcae970a0384a20
Harshul <88796921+harshul26@users.noreply.github.com>
2022-09-18 21:00:32 +0530

    Add files via upload

---
+++ easy/moveZeroesToEnd.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int i = 0 , j = -1;
+        while(i < n){
+            if(nums[i] == 0 && j == -1)
+                    j = i;
+            else if(nums[i] != 0 && j != -1){
+                nums[j] = nums[i];
+                nums[i] = 0;
+                j++;
+            }
+            i++;
+        }
+    }
+}
